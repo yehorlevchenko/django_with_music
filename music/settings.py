258ed62d3b3dc27f5726fb7e499d4be288c0a1d5
Yehor Levchenko <yehorlevchenko@icloud.com>
2020-06-19 01:39:06 +0300

    dotenv added

--- music/settings.py
+++ music/settings.py
@@ -20,14 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'z5kt4zw3z&waq(033i(qj+ij$mox!s_g9r_4esr=!=mt(khc77'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-# ALLOWED_HOSTS = ['0.0.0.0']
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(' ')
 
 # Application definition
