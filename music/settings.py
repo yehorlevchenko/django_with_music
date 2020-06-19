18d8a6c72febea3118200c0ebf7cbe108ab555a7
Yehor Levchenko <yehorlevchenko@icloud.com>
2020-06-19 15:58:59 +0300

    app added

--- music/settings.py
+++ music/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(' ')
 # Application definition
 
 INSTALLED_APPS = [
+    'works_single_view.apps.MusicViewConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
