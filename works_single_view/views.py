3f50cedc75dd921cd8b7cf5d9b4bfc043e0350b0
Yehor Levchenko <yehorlevchenko@icloud.com>
2020-06-20 17:34:12 +0300

    Upd for informative view

--- works_single_view/views.py
+++ works_single_view/views.py
@@ -3,21 +3,23 @@ from django.template import loader
 from django.shortcuts import reverse
 
 from .models import Work, Contributor
-from .utils import parse_works, save_works_from_csv
+from .utils import parse_works, save_works
 
 
 def index(request):
     template = loader.get_template('index.html')
-    context = {}
     if request.method == 'GET':
+        works_list = Work.objects.all()
+        context = {'works_list': works_list}
         return HttpResponse(template.render(context, request))
+    else:
+        in_file = request.FILES.get('csv_file')
+        if not in_file:
+            return HttpResponseRedirect(reverse('index'))
 
-    in_file = request.FILES.get('csv_file')
-    if not in_file:
-        return HttpResponseRedirect(reverse('index'))
+        if in_file.name.endswith('.csv'):
+            data = in_file.read().decode('utf-8')
+            raw_works_list = parse_works(data)
+            save_works(raw_works_list)
 
-    if in_file.name.endswith('.csv'):
-        data = in_file.read().decode('utf-8')
-        raw_works_list = parse_works(data)
-        save_works_from_csv(raw_works_list)
         return HttpResponseRedirect(reverse('index'))
